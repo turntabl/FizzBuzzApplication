e29782d301d750e3e5e139595e8e767ce0e039ab
Dilyen <davidandohacquah@gmail.com>
2021-10-18 15:35:38 +0000

    testPositiveIntegerReturnFizz

--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -2,4 +2,12 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class FizzBuzzTest{
+    private val testFizzBuzz: FizzBuzz = FizzBuzz()
+
+    @Test
+    fun testPositiveIntegerReturnFizz() {
+        val results = testFizzBuzz.getFizzBuzz(3)
+        val expected = "fizz"
+        assertEquals(expected, results)
+    }
 }
